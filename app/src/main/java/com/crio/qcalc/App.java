82f8179f307a3fffc19d185dc7f709093d2fc980
CrioUser <crio-user@criodo.com>
2023-01-10 20:44:23 +0530

    ME_QCALC_QA_MODULE_IMPLEMENTATION

--- app/src/main/java/com/crio/qcalc/App.java
+++ app/src/main/java/com/crio/qcalc/App.java
@@ -3,12 +3,25 @@
  */
 package com.crio.qcalc;
 
+
 public class App {
+
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+   public static void main(String[] args) {
+
+       System.out.println("Starting QCalc..");
+
+       StandardCalculator calc = new StandardCalculator();
+
+       calc.add(1, 2);
+
+       System.out.println(calc.getResult());
+
+   }
+
+
 }
+
