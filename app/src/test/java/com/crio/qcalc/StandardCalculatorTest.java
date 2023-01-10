1255ae1d893624b2261b76909b47f4f874dfc91b
CrioUser <crio-user@criodo.com>
2023-01-10 21:07:44 +0530

    ME_QCALC_QA_MODULE_OOP

--- app/src/test/java/com/crio/qcalc/StandardCalculatorTest.java
+++ app/src/test/java/com/crio/qcalc/StandardCalculatorTest.java
@@ -13,26 +13,35 @@ public class StandardCalculatorTest {
         standardCalculator = new StandardCalculator();
     }
 
+@Test
 
+@DisplayName("Test Addition of Two Integers")
 
+void testAdditionOperation(){
 
-    @Test
-    @DisplayName("Test Addition of Two Integers")
-    void testAdditionOperation(){
         standardCalculator.add(1,1);
-        int actualResult = standardCalculator.getResult();
+
+        double actualResult = standardCalculator.getResult();
+
         Assertions.assertEquals(2, actualResult);
-    }
 
-    @Test
-    @DisplayName("Test Substraction of Two Integers")
-    void testSubtractionOperation(){
+}
+
+
+@Test
+
+@DisplayName("Test Subtraction of Two Integers")
+
+void testSubtractionOperation(){
+
         standardCalculator.subtract(1,1);
-        int actualResult = standardCalculator.getResult();
-        Assertions.assertEquals(0, actualResult);
-    }
 
-    
+        double actualResult = standardCalculator.getResult();
+
+        Assertions.assertEquals(0, actualResult);
+
+}
+
 @Test
 
 @DisplayName("Test Division of Two Integers")
@@ -40,7 +49,7 @@ public class StandardCalculatorTest {
 void testDivisionOperation()
 {
     standardCalculator.divide(1,1);
-        int actualResult = standardCalculator.getResult();
+        double actualResult = standardCalculator.getResult();
         Assertions.assertEquals(1, actualResult);
 
 }
@@ -53,12 +62,67 @@ void testDivisionOperation()
 void testMultiplicationOperation()
 {
     standardCalculator.multiply(1,1);
-    int actualResult = standardCalculator.getResult();
+    double actualResult = standardCalculator.getResult();
     Assertions.assertEquals(1, actualResult);
 
 }
 
 
+@Test
+
+@DisplayName("Test Addition of Two Doubles")
+
+void testAdditionOperationForDoubles(){
+
+        standardCalculator.add(1.0,1.5);
+
+        double actualResult = standardCalculator.getResult();
+
+        Assertions.assertEquals(2.5, actualResult);
+
+}
+
+
+@Test
+
+@DisplayName("Test Subtraction of Two Doubles")
+
+void testSubtractionOperationForDoubles(){
+
+        standardCalculator.subtract(10.0,20.5);
+
+        double actualResult = standardCalculator.getResult();
+
+        Assertions.assertEquals(-10.5, actualResult);
+
+}
+
+
+
+@Test
+
+@DisplayName("Test Multiplication of Two Doubles")
+
+void testMultiplicationOperationForDoubles()
+{
+    standardCalculator.multiply(1.2,2.2);
+    double actualResult = standardCalculator.getResult();
+    Assertions.assertEquals(2.64, actualResult);
+
+
+}
+
+
+@Test
+
+@DisplayName("Test Division of Two Doubles")
+
+void testDivisionOperationForDoubles()
+{
+    standardCalculator.divide(1.0,1.0);
+    double actualResult = standardCalculator.getResult();
+    Assertions.assertEquals(1.0, actualResult);
+}
 
 
 }
